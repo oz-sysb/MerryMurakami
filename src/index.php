1fef8c5e9dc27017be54f7466a6c88b6925243fb
y-murakami <y-murakami@oz-vision.co.jp>
2015-07-10 22:30:15 +0900

    リポジトリがさびしいので何か上げる
    自販機デモ画面。

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+
+</head>
+<body>
+<h1>自販機デモ画面</h1>
+<form>
+	<select name="money">
+		<option>1円玉</option>
+		<option>5円玉</option>
+		<option>10円玉</option>
+		<option>50円玉</option>
+		<option>100円玉</option>
+		<option>500円玉</option>
+		<option>1,000円玉</option>
+		<option>2,000円玉</option>
+		<option>5,000円玉</option>
+		<option>10,000円玉</option>
+	</select>
+	<input type="submit">
+</form>
+</body>
+</html>
