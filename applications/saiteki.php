d161cd1af7f97a8129c3f57a84aabc076588dde8
oomurata <09ra193@gmail.com>
2015-07-12 20:45:58 +0900

    [modify]保有額を確かめる条件を追加

--- applications/saiteki.php
+++ applications/saiteki.php
@@ -15,13 +15,24 @@ class Saiteki
      * @param int   $total       おつりの総額
      * @param array $hold_moneys 保有しているお金
      *
-     * @return array 余ったおつり，おつりの組み合わせ
+     * @return array 余ったおつりとおつりの組み合わせを持つリスト
      */
     public function saiteki($total, $hold_moneys)
     {
+        // 保有しているお金のkey名を降順で取得する
+        $money_keys = array_keys($hold_moneys);
+        rsort($money_keys);
+
+        // 返すおつりのリスト
         $change = array();
-        foreach (array(1000, 500, 100, 50, 10) as $coin)
+        foreach ($money_keys as $coin)
         {
+            // 保有数が0以下ならば調べない
+            if($hold_moneys[$coin] <= 0)
+            {
+                continue;
+            }
+            // 総額が調べる対象のお金よりも大きいならループする
             for (; $total >= $coin; $total -= $coin)
             {
                 array_push($change, $coin);
