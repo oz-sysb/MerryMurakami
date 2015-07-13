bb91e1d33bc771c79aad6b97c45242e1e27f381b
y-murakami <y-murakami@oz-vision.co.jp>
2015-07-13 20:47:36 +0900

    判別さんのテストと実装
    1円から全部requireするの面倒なのでautoloadも追加してみた。

---
+++ applications/config/autoload.php
@@ -0,0 +1,6 @@
+<?php
+require_once(dirname(__FILE__) . '/../money.php');
+require_once(dirname(__FILE__) . '/../oneyen.php');
+require_once(dirname(__FILE__) . '/../fiveyen.php');
+require_once(dirname(__FILE__) . '/../tenyen.php');
+
