e414541683e3d02b6a9773e996d4ddc05e2cdc28
nagayamashouta <s-nagayama@oz-vision.co.jp>
2015-07-13 20:57:01 +0900

    いれとくさん作成 ユニットテストだけかいた

--- tests/iretokuTest.php
+++ tests/iretokuTest.php
@@ -1,15 +1,49 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: y-murakami
- * Date: 2015/07/10
- * Time: 22:59
- */
+require_once(dirname(__FILE__) . '/../applications/iretoku.php');
 
 class iretokuTest extends PHPUnit_Framework_TestCase
 {
-	public function testFuga()
-	{
+	private $iretoku_obj;
 
+	/**
+	 * 初期化関数
+	 */
+	public function setup()
+	{
+		$this->iretoku = new Iretoku();
+	}
+
+	/**
+	 *
+	 */
+	public function testIretokuOK()
+	{
+		$this->assertEquals(TRUE, $this->iretoku_obj->iretoku($okane_obj));
+	}
+
+	/**
+	 *
+	 */
+	public function testIretokuNG()
+	{
+		$this->assertEquals(FALSE, $this->iretoku_obj->iretoku($okane_obj));
+	}
+
+	/**
+	 *
+	 */
+	public function testIwaretabundasuOK()
+	{
+		$result_okane_obj = $this->iwaretabundasu($kingaku);
+		$this->assertEquals($ok_okane_obj, $result_okane_obj);
+	}
+
+	/**
+	 *
+	 */
+	public function testIwaretabundasuNG()
+	{
+		$result_okane_obj = $this->iwaretabundasu($kingaku);
+		$this->assertEquals($ng_okane_obj, $result_okane_obj);
 	}
 }
