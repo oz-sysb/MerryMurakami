5c1439318b056395d07aa1020892f41022c4e069
oomurata <09ra193@gmail.com>
2015-07-13 21:52:46 +0900

    [modify]ユニットテストを修正

--- tests/iretokuTest.php
+++ tests/iretokuTest.php
@@ -1,51 +1,54 @@
 <?php
 require_once(dirname(__FILE__) . '/../applications/iretoku.php');
-
 class iretokuTest extends PHPUnit_Framework_TestCase
 {
 	private $iretoku_obj;
-	private $ok_okane;
-
 	/**
 	 * 初期化関数
+	 *
+	 * @return void
 	 */
 	public function setup()
 	{
-		$this->iretoku_obj = new Iretoku();
+		$this->$iretoku_obj = new Iretoku();
 	}
-
 	/**
+	 * 入れとく関数 正常系
 	 *
+	 * @return void
 	 */
-	public function testIretokuOK()
+	public function testIretokuOK($okane_obj)
 	{
-		$this->assertEquals(TRUE, $this->iretoku_obj->iretoku($okane_obj));
+		$result = $this->iretoku_obj->iretoku($okane_obj);
+		$this->assertEquals(TRUE, $result);
 	}
-
 	/**
+	 * 入れとく関数 異常系
 	 *
+	 * @return void
 	 */
-	public function testIretokuNG()
+	public function testIretokuNG($okane_obj)
 	{
-		$this->assertEquals(FALSE, $this->iretoku_obj->iretoku($okane_obj));
+		$result = $this->iretoku_obj->iretoku($okane_obj);
+		$this->assertEquals(TRUE, $result);
 	}
-
 	/**
+	 * 言われた分出す 正常系
 	 *
+	 * @return void
 	 */
-	public function testIwaretabundasuOK()
+	public function testIwaretabundasuOK($kingaku, $ok_okane_obj)
 	{
-		$ok_okane_obj = new Tenyen();
 		$result_okane_obj = $this->iwaretabundasu($kingaku);
 		$this->assertEquals($ok_okane_obj, $result_okane_obj);
 	}
-
 	/**
+	 * 言われた分出す 異常系
 	 *
+	 * @return void
 	 */
-	public function testIwaretabundasuNG()
+	public function testIwaretabundasuNG($kingaku, $ng_okane_obj)
 	{
-		$ng_okane_obj = new Fiveyen();
 		$result_okane_obj = $this->iwaretabundasu($kingaku);
 		$this->assertEquals($ng_okane_obj, $result_okane_obj);
 	}
