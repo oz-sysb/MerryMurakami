0a4d0342218f2b350585c6c55758b1b824b106d8
nagayamashouta <s-nagayama@oz-vision.co.jp>
2015-07-16 20:54:59 +0900

    再設計の際にいなくなったキャラクターを削除 お金マン 計算君 最適君 操作番長 はきだすちゃん 表示君

--- applications/oneyen.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("../applications/money.php");
-class Oneyen extends money
-{
-	function __construct()
-	{
-		$this->amount = 1;
-	}
-}
--- applications/tenthousandyen.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("../applications/money.php");
-class Tenthousandyen extends money
-{
-	function __construct()
-	{
-		$this->amount = 10000;
-	}
-}
