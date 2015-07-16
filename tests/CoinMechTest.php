878c3818787ae8f7057b05e14a84dabba4f61bef
y-murata <y-murata@oz-vision.co.jp>
2015-07-16 21:20:17 +0900

    [modify]テスト項目を追加しました
    trueが引数のときredなります

--- tests/CoinMechTest.php
+++ tests/CoinMechTest.php
@@ -3,34 +3,42 @@ require_once("../applications/CoinMech.php");
 
 class CoinMechTest extends PHPUnit_Framework_TestCase
 {
-    private $CoinMech;
+	private $CoinMech;
 
-    public function setUp()
-    {
-        $this->CoinMech = new CoinMech();
-    }
+	public function setUp()
+	{
+		$this->CoinMech = new CoinMech();
+	}
 
-    /**
-     * @test
-     *
-     * @dataProvider moneyProvider
-     */
-    public function 使える硬貨かどうか($amount, $expected)
-    {
-        $this->assertEquals($expected, $this->CoinMech->is_usable($amount));
-    }
+	/**
+	 * @test
+	 *
+	 * @dataProvider moneyProvider
+	 */
+	public function 使える硬貨かどうか($amount, $expected)
+	{
+		$this->assertEquals($expected, $this->CoinMech->is_usable($amount));
+	}
 
-    public function moneyProvider()
-    {
-        return [[1, false],
-                [5, false],
-                [10, true],
-                [50, true],
-                [100, true],
-                [500, true],
-                [1000, true],
-                [2000, false],
-                [5000, false],
-                [10000, false]];
-    }
+	public function moneyProvider()
+	{
+		return [
+			[     1, false],
+			[     5, false],
+			[    10,  true],
+			[    50,  true],
+			[   100,  true],
+			[   500,  true],
+			[  1000,  true],
+			[  2000, false],
+			[  5000, false],
+			[ 10000, false],
+			['hoge', false],
+			[	  0, false],
+			[	 -1, false],
+			[  true, false], //ここがおかしい
+			[ false, false],
+			[  null, false],
+		];
+	}
 }
