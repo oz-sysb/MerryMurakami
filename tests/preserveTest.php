9d86019b958306957eda43147557aa21d9f43e41
y-murata <y-murata@oz-vision.co.jp>
2015-07-17 21:14:30 +0900

    [modify]@testの形式に変更
    関数名の先頭からtestの文字を消すことができる

--- tests/preserveTest.php
+++ tests/preserveTest.php
@@ -16,6 +16,8 @@ class PreserveTest extends PHPUnit_Framework_TestCase
 	}
 
 	/**
+	 * @test
+	 *
 	 * @param int $invest   投入金額
 	 * @param int $expected 投入されている総額
 	 *
@@ -23,13 +25,15 @@ class PreserveTest extends PHPUnit_Framework_TestCase
 	 *
 	 * @dataProvider 総額に加える_正常系Provider
 	 */
-	public function test総額に加える_正常系($invest, $expected)
+	public function 総額に加える_正常系($invest, $expected)
 	{
 		$result = $this->preserve_obj->add_amount($invest);
 		$this->assertEquals($expected, $result);
 	}
 
 	/**
+	 * @test
+	 *
 	 * @param int $invest   投入金額
 	 * @param int $expected 投入されている総額
 	 *
@@ -37,10 +41,50 @@ class PreserveTest extends PHPUnit_Framework_TestCase
 	 *
 	 * @dataProvider 総額に加える_異常系Provider
 	 */
-	public function test総額に加える_異常系($invest, $expected)
+	public function 総額に加える_異常系($invest, $expected)
 	{
 		$result = $this->preserve_obj->add_amount($invest);
 		$this->assertEquals($expected, $result);
+	}
+
+	/**
+	 * @test
+	 *
+	 * @param array $invests  投入金額の配列
+	 * @param int   $expected 取り出された総額
+	 *
+	 * @return void
+	 *
+	 * @dataProvider 総額を取り出す_正常系Provider
+	 */
+	public function 総額を取り出す_正常系($invests, $expected)
+	{
+		foreach($invests as $invest)
+		{
+			$this->preserve_obj->add_amount($invest);
+		}
+		$result = $this->preserve_obj->take_out_amount();
+		$this->assertEquals($expected, $result);
+	}
+
+	/**
+	 * @test
+	 *
+	 * @param array $invests  投入金額の配列
+	 * @param int   $expected 取り出された総額
+	 *
+	 * @return void
+	 *
+	 * @dataProvider 総額を教える_正常系Provider
+	 */
+	public function 総額を教える_正常系($invests, $expected)
+	{
+		for($i = 0; $i < count($invests); $i++)
+		{
+			$this->preserve_obj->add_amount($invests[$i]);
+			$result = $this->preserve_obj->get_amount();
+			$this->assertEquals($expected[$i], $result);
+		}
 	}
 
 	/**
@@ -82,24 +126,6 @@ class PreserveTest extends PHPUnit_Framework_TestCase
 	}
 
 	/**
-	 * @param array $invests  投入金額の配列
-	 * @param int   $expected 取り出された総額
-	 *
-	 * @return void
-	 *
-	 * @dataProvider 総額を取り出す_正常系Provider
-	 */
-	public function test総額を取り出す_正常系($invests, $expected)
-	{
-		foreach($invests as $invest)
-		{
-			$this->preserve_obj->add_amount($invest);
-		}
-		$result = $this->preserve_obj->take_out_amount();
-		$this->assertEquals($expected, $result);
-	}
-
-	/**
 	 * データプロバイダ
 	 * 総額を取り出す
 	 *
@@ -125,24 +151,6 @@ class PreserveTest extends PHPUnit_Framework_TestCase
 				150
 			],
 		];
-	}
-
-	/**
-	 * @param array $invests  投入金額の配列
-	 * @param int   $expected 取り出された総額
-	 *
-	 * @return void
-	 *
-	 * @dataProvider 総額を教える_正常系Provider
-	 */
-	public function test総額を教える_正常系($invests, $expected)
-	{
-		for($i = 0; $i < count($invests); $i++)
-		{
-			$this->preserve_obj->add_amount($invests[$i]);
-			$result = $this->preserve_obj->get_amount();
-			$this->assertEquals($expected[$i], $result);
-		}
 	}
 
 	/**
