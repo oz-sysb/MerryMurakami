1fe6936aacd9f3af8897ac0d4de0977506112056
ノーカ <noka.blank@gmail.com>
2015-07-17 21:26:12 +0900

    名称の修正もれ
    * コインメック（CoinMech）に統一

--- tests/CoinMechTest.php
+++ tests/CoinMechTest.php
@@ -3,64 +3,60 @@ require_once("../applications/CoinMech.php");
 
 class CoinMechTest extends PHPUnit_Framework_TestCase
 {
-    private $CoinMech;
+	private $CoinMech;
 
-    public function setUp()
-    {
-        $this->CoinMech = new CoinMech();
-    }
+	public function setUp()
+	{
+		$this->CoinMech = new CoinMech();
+	}
 
-    /**
-     * @test
-     *
-     * @param $amount
-     * @param $expected
-     *
-     * @dataProvider moneyProvider
-     */
-    public function 使える硬貨かどうか($amount, $expected)
-    {
-        $this->assertEquals($expected, $this->CoinMech->is_usable($amount));
-    }
+	/**
+	 * @test
+	 *
+	 * @param $amount
+	 * @param $expected
+	 *
+	 * @dataProvider moneyProvider
+	 */
+	public function 使える硬貨かどうか($amount, $expected)
+	{
+		$this->assertEquals($expected, $this->CoinMech->is_usable($amount));
+	}
 
-    /**
-     * @test
-     *
-     * @param $amount
-     * @param $expected
-     *
-     * @dataProvider illegalProvider
-     */
-    public function 数値以外はfalseになるか($amount, $expected)
-    {
-        $this->assertEquals($expected, $this->CoinMech->is_usable($amount));
-    }
+	/**
+	 * @test
+	 *
+	 * @param $amount
+	 * @param $expected
+	 *
+	 * @dataProvider illegalProvider
+	 */
+	public function 数値以外はfalseになるか($amount, $expected)
+	{
+		$this->assertEquals($expected, $this->CoinMech->is_usable($amount));
+	}
 
-    public function moneyProvider()
-    {
-        return [
-            [1, false],
-            [5, false],
-            [10, true],
-            [50, true],
-            [100, true],
-            [500, true],
-            [1000, true],
-            [2000, false],
-            [5000, false],
-            [10000, false]
-        ];
-    }
+	public function moneyProvider()
+	{
+		return [[1, false],
+				[5, false],
+				[10, true],
+				[50, true],
+				[100, true],
+				[500, true],
+				[1000, true],
+				[2000, false],
+				[5000, false],
+				[10000, false]];
+	}
 
-    public function illegalProvider()
-    {
-        return [
-            ['hoge', false],
-            [0, false],
-            [-1, false],
-            [true, false],
-            [false, false],
-            [null, false]
-        ];
-    }
+	public function illegalProvider()
+	{
+		return [['hoge', false],
+				[0, false],
+				[-1, false],
+				[true, false],
+				[false, false],
+				[null, false]];
+	}
 }
