f79801b411471a0eef7f01dfe28068559e406045
y-murata <y-murata@oz-vision.co.jp>
2015-07-20 19:09:04 +0900

    [modify]autoloadされるファイルを追加

--- applications/config/autoload.php
+++ applications/config/autoload.php
@@ -1,3 +1,5 @@
 <?php
 require_once(dirname(__FILE__) . "/../CoinMech.php");
 require_once(dirname(__FILE__) . "/../preserve.php");
+require_once(dirname(__FILE__) . "/../ItemManager.php");
+require_once(dirname(__FILE__) . "/../vendingMachìne.php");
